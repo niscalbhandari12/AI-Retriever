2b86a92fd3b392a19c8a24522abd58db83a2e84b
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-04-07 10:14:59 +0545

    added changes

--- main.py
+++ main.py
@@ -1,91 +1,95 @@
-import bs4
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain_community.llms import Ollama
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import RetrievalQA
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline,AutoModelForSeq2SeqLM
-from Retriever import CustomRetriever
+
+# First we need to import spacy 
+import spacy 
+
+import  MysqlQueries.getTokenForQuestion
+import MysqlQueries.getFirstLevelData
+# Creating blank language object then 
+# tokenizing words of the sentence 
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("what is phone of nischal ?") 
+verbs = []
+auxillaryVerbs = []
+adjectives = []
+pronouns = []
+punctuations = []
+nouns = []
+sconj = []
+propn = []
+
+alltokens = []
+def switchTokens(token):
+    if token.pos_ == "VERB":
+        verbs.append(token)
+    elif token.pos_ == "AUX":
+        auxillaryVerbs.append(token)
+    elif token.pos_ == "ADJ":
+        adjectives.append(token)
+    elif token.pos_ == "PRON":
+        pronouns.append(token)
+    elif token.pos_ == "PUNCT":
+        punctuations.append(token)
+    elif token.pos_ == "NOUN":
+        nouns.append(token)
+    elif token.pos_ == "SCONJ":
+        sconj.append(token)
+    elif token.pos_ == "PROPN":
+        propn.append(token)
+
+for token in doc:
+    switchTokens(token)
+    print(token.pos_)
+
+for val in adjectives:
+    alltokens.append(MysqlQueries.getTokenForQuestion.getToken(val.text))
+# for val in nouns:
+#     print(val)
+#     alltokens.append(MysqlQueries.getTokenForQuestion.getToken(val.text))
+
+for val in alltokens:
+    if len(nouns) > 0:
+
+        vals = MysqlQueries.getFirstLevelData.getFirstLevelData(val, nouns[0].text)
+        print(vals)
+    
+for val in nouns:
+    alltokens.append(MysqlQueries.getTokenForQuestion.getToken(val.text))
+    # for val in nouns:
+    #     print(val)
+    #     alltokens.append(MysqlQueries.getTokenForQuestion.getToken(val.text))
+
+for val in alltokens:
+    if len(adjectives) > 0:
+
+        vals = MysqlQueries.getFirstLevelData.getFirstLevelData(val, adjectives[0].text)
+        print(vals)
 
 
-model_id = 'google/flan-t5-small'
-#model_id = 'facebook/blenderbot-1B-distill'
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
-template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Context: {context}
-Question: {question}
-Only return the helpful answer below and nothing else.
-Helpful answer:
-"""
+# print("verbs : ") 
+# print(verbs) 
+# print("\n")
+# print("auxillary verbs : " )
+# print(auxillaryVerbs)
+# print("\n")
+# print("adjectives : "  )
+# print(adjectives)
+# print("\n")
+# print("pronouns : "  )
+# print(pronouns)
+# print("\n")
+# print("noun : ")
+# print(nouns)
+# print("\n")
+# print("punctuations : ")
+# print(punctuations)
+# print("\n")
+# print("subordinating conjunction : ")
+# print(sconj)
+# print("\n")
+# print("Proper noun : ")
+# print(propn)
+# print("\n")
 
-secondtemplate= """Question: {question}
-Answer: Let's think step by step.
-"""
-prompt1 = PromptTemplate(template=secondtemplate, input_variables=["question"])
-#llm = Ollama(model="llama2")
-
-pipe = pipeline(
-    'text2text-generation',
-    model = model, 
-    tokenizer=tokenizer,
-    max_length=1000
-)
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'})
-db = FAISS.load_local("faiss", embeddings,allow_dangerous_deserialization=True)
-retriever = db.as_retriever(search_kwargs={'k': 3})
-
-local_llm = HuggingFacePipeline(pipeline=pipe)
-
-# llm_chain = LLMChain(llm=local_llm,
-#                         prompt=prompt1)
-
-#print(llm_chain.run('who is nischal? '))
-
-
-
-
-# embeddings = HuggingFaceEmbeddings(
-#     model_name="sentence-transformers/all-MiniLM-L6-v2",
-#     model_kwargs={'device': 'cpu'})
-
-
-prompt = PromptTemplate(
-    template=template,
-    input_variables=['context', 'question'])
-
-customRetriever = CustomRetriever()
-qa_llm = RetrievalQA.from_chain_type(llm=local_llm,
-                                     chain_type='stuff',
-                                     retriever=customRetriever,
-                                     return_source_documents=True,
-                                     chain_type_kwargs={'prompt': prompt})
-
-
-# prompt = "Who has worked in ERP system? What is their favorite color?"
-# output = qa_llm({'query': prompt})
-# print(output["result"])
-
-def get_prompt():
-    print("Type 'exit' to quit")
-
-    while True:
-        prompt = input("Enter a prompt: ")
-
-        if prompt.lower() == 'exit':
-            print('Exiting...')
-            break
-        else:
-            try:
-                print(retriever.get_relevant_documents(prompt))
-                output = qa_llm({'query': prompt})
-                print(output["result"])
-            except Exception as e:
-                print(e)
-
-get_prompt()
+# print("Freeschema Tokens : ")
+# print(alltokens)
+# print("\n")
